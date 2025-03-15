cd4cc8997359d384e9deb8cdb09b523c27826568
Wladson14 <110068714+Wladson14@users.noreply.github.com>
2025-03-14 23:49:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const botao = document.querySelector('.adicionar')
+const texto = document.querySelector('.tarefa')
+const listacomp = document.querySelector('.lista')
+
+let lista = []
+
+function adicionar() {
+    lista.push({
+        item: texto.value,
+        concluida: false
+    })
+    
+    texto.value = ''
+
+   
+    aparecer_na_tela()
+
+    
+    if (texto.value == 0) {
+        alert('erro')
+    }
+}
+
+function aparecer_na_tela() {
+
+    let task = ''
+
+    lista.forEach((tarefa, posicao) => {
+        task = task + `
+          <li class="task ${tarefa.concluida && "done"}">
+                <img  src="./img/checked.png" alt="" onclick="concluir(${posicao})">
+                <p>${tarefa.item}</p>
+                <img src="./img/trash.png" alt="" onclick="deletar(${posicao})">
+            </li>
+        `
+   
+    })
+
+
+    listacomp.innerHTML = task
+
+    localStorage.setItem('list', JSON.stringify(lista))
+    
+}
+
+function concluir(posicao) {
+   lista[posicao].concluida= !lista[posicao].concluida
+
+   aparecer_na_tela()
+}
+
+function deletar(posicao) {
+    lista.splice(posicao, 1)
+
+    aparecer_na_tela()
+   
+}
+
+function recarregar() {
+    const taskls = localStorage.getItem('list')
+
+
+    if (taskls) {
+        lista = JSON.parse(taskls)
+    }
+
+    aparecer_na_tela()
+}
+
+recarregar()
+botao.addEventListener('click', adicionar)
